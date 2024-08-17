f9e46921667efa2b020304f97c831ca020bf7050
hazecodeio <3461124+hazecodeio@users.noreply.github.com>
2024-08-17 13:22:53 -0500

    committing changes..

--- _forks/spark-scala-examples/src/main/scala/com/sparkbyexamples/spark/dataframe/JsonFromMultiline.scala
+++ _forks/spark-scala-examples/src/main/scala/com/sparkbyexamples/spark/dataframe/JsonFromMultiline.scala
@@ -4,15 +4,17 @@ import org.apache.spark.sql.SparkSession
 
 object JsonFromMultiline extends App {
 
-  val spark: SparkSession = SparkSession.builder()
-    .master("local[3]")
-    .appName("SparkByExamples.com")
-    .getOrCreate()
+    val spark: SparkSession = SparkSession.builder()
+        .master("local[3]")
+        .appName("SparkByExamples.com")
+        .getOrCreate()
 
-  //read multiline json file
-  val multiline_df = spark.read.option("multiline", "true")
-    .json("src/main/resources/multiline-zipcode.json")
-  multiline_df.printSchema()
-  multiline_df.show(false)
+    val base_data_dir = s"file://${System.getProperty("user.dir")}/_forks/spark-scala-examples/"
+
+    //read multiline json file
+    val multiline_df = spark.read.option("multiline", "true")
+        .json(s"$base_data_dir/src/main/resources/multiline-zipcode.json")
+    multiline_df.printSchema()
+    multiline_df.show(false)
 
 }
